72a4e90f23c7447bc57799b986bef3c01028fb92
jeanmichelr7 <michmich_68@hotmail.fr>
2021-06-24 14:20:28 +0200

    front: signup login

---
+++ frontend/groupomania-app/src/views/Login.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="hello">
+    <section>
+      <h1>Connexion</h1>
+      <form class="form-login">
+          <div class="form-row">
+
+              <!-- Email-->
+              <div class="form-group col-md-6">
+                  <label for="inputEmail">Email :</label>
+                  <input type="email" class="form-control" id="inputEmail" required
+                      placeholder="exemple@messagerie.fr">
+              </div>
+
+              <!-- Ville -->
+              <div class="form-group col-md-6">
+                  <label for="inputPassword">Mot de passe :</label>
+                  <input type="password" class="form-control" id="inputPassword" required>
+              </div>
+          </div>
+
+          <!-- sauvegarde donné-->
+          <div class="form-group">
+              <div class="form-check">
+                  <input class="form-check-input" type="checkbox" id="gridCheck">
+                  <label class="form-check-label" for="gridCheck">
+                      Sauvegardez les informations
+                  </label>
+              </div>
+          </div>
+          
+          <button id="btnSubmit" type="button" class="btn btn-primary">
+              Entrer
+          </button>
+      </form>
+    </section>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.hello{
+  border: black solid 2px
+}
+</style>
---
+++ frontend/groupomania-app/src/views/Signup.vue
@@ -0,0 +1,81 @@
+<template>
+    <form enctype="multipart/form-data" action="login.html" class="form-login">
+        <fieldset>
+            <legend>Inscription</legend>
+            <p>Rentrer les informations pour créer un compte utilisateur</p>
+
+            <!-- Nom / Prenom -->
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="inputFirstName">Nom : </label>
+                    <input type="text" class="form-control" placeholder="Nom" name="nom"
+                        id="inputFirstName" required>
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="inputLastName">Prénom :</label>
+                    <input type="text" class="form-control" placeholder="Prénom" id="inputLastName"
+                        required>
+                </div>
+            </div>
+            <div class="form-row">
+
+                <!-- Email-->
+                <div class="form-group col-md-6">
+                    <label for="inputEmail">Email</label>
+                    <input type="email" class="form-control" id="inputEmail" required
+                        placeholder="exemple@messagerie.fr">
+                </div>
+
+                <!-- Ville -->
+                <div class="form-group col-md-6">
+                    <label for="inputPassword">Mot de passe</label>
+                    <input type="password" class="form-control" id="inputPassword" required>
+                </div>
+            </div>
+
+            <!-- Accepter les conditions -->
+            <div class="form-group">
+                <div class="form-check">
+                    <input class="form-check-input is-invalid" type="checkbox" value=""
+                        id="invalidCheck30" required>
+                    <label class="form-check-label" for="invalidCheck30">
+                        Agree to terms and conditions
+                    </label>
+                    <div class="invalid-feedback">
+                        You must agree before submitting.
+                    </div>
+                </div>
+            </div>
+            
+            <!-- sauvegarde donné-->
+            <div class="form-group">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="gridCheck">
+                    <label class="form-check-label" for="gridCheck">
+                        Sauvegardez les informations
+                    </label>
+                </div>
+            </div>
+            <button id="btnSubmit" type="button" class="btn btn-primary">
+                S'enregistrer
+            </button>
+        </fieldset>
+    </form>
+</template>
+
+
+
+<style scoped>
+.form-login{
+  width:80%;
+  border:solid rgb(151, 151, 151) 1px;
+  border-radius: 5px;
+  box-shadow: 2px 2px 2px 2px #757575;
+
+  margin-top:20px;
+  margin-bottom:30px;
+  padding:20px;
+}
+
+
+</style>
